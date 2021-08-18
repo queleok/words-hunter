05a92ef5c2a759a351b5c03e9da3be5dc3c0c222
Leo K <queleok@gmail.com>
2021-08-19 01:16:51 +0300

    added test case verifying failed words are added correctly; updated ts-jest

--- src/scripts/test/e2e/word-validation.test.ts
+++ src/scripts/test/e2e/word-validation.test.ts
@@ -1,4 +1,4 @@
-import { ElementHandle } from 'puppeteer';
+import { ElementHandle, HTTPRequest } from 'puppeteer';
 
 const timeout = 10000;
 
@@ -12,7 +12,8 @@ beforeAll(async () => {
 
 test('Ensure validated words are added correctly', async () => {
     await page.setRequestInterception(true);
-    page.on('request', request => {
+
+    const success_handler = (request: HTTPRequest)  => {
         if (request.url().startsWith('https://api.dictionaryapi.dev/api/v2/entries/en/')) {
             const word = request.url().split('/').pop();
             request.respond({
@@ -22,7 +23,8 @@ test('Ensure validated words are added correctly', async () => {
                 body: `[{ "word": "${word}", "meanings": [ { "partOfSpeech": "stub", "definitions": [ { "definition": "stub" } ]}]}]`
             });
         } else request.continue();
-    });
+    };
+    page.on('request', success_handler);
 
     const letters = await page.$$('.cell');
 
@@ -56,4 +58,62 @@ test('Ensure validated words are added correctly', async () => {
     const second_word_eh = (await page.$('.success'))!;
     const published_second_word = (await getPropertyUnsafe(second_word_eh, 'textContent'))!;
     expect(published_second_word).toBe(second_word);
+
+    page.off('request', success_handler);
+
+    await page.setRequestInterception(false);
+}, timeout);
+
+test('Ensure failed words are added correctly', async () => {
+    await page.reload();
+    await page.setRequestInterception(true);
+    const failure_handler = (request: HTTPRequest) => {
+        if (request.url().startsWith('https://api.dictionaryapi.dev/api/v2/entries/en/')) {
+            const word = request.url().split('/').pop();
+            request.respond({
+                status: 404,
+                headers: { "Access-Control-Allow-Origin": "*" },
+                contentType: 'application/json',
+                body: `[ "Word '${word}' not found" ]`
+            });
+        } else request.continue();
+    };
+    page.on('request', failure_handler);
+
+    const letters = await page.$$('.cell');
+
+    const send_first_n_letters = async (n: number): Promise<string> => {
+        let counter = 0;
+        let word = '';
+        for (const letter of letters) {
+            await letter.click();
+            word += await getPropertyUnsafe(letter, 'textContent');
+            counter++;
+            if (counter == n) break;
+        }
+        const send = await page.$('#publish');
+        await send!.click();
+
+        return word;
+    };
+
+    const first_word = (await send_first_n_letters(3))!;
+
+    const first_word_eh = (await page.waitForSelector('.failure'))!;
+    const published_first_word = (await getPropertyUnsafe(first_word_eh, 'textContent'))!;
+    expect(published_first_word).toBe(first_word);
+
+    const second_word = (await send_first_n_letters(4))!;
+
+    const moved_first_word_eh = (await page.waitForSelector('.failure ~ .failure'))!;
+    const moved_published_first_word = (await getPropertyUnsafe(moved_first_word_eh, 'textContent'))!;
+    expect(moved_published_first_word).toBe(first_word);
+
+    const second_word_eh = (await page.$('.failure'))!;
+    const published_second_word = (await getPropertyUnsafe(second_word_eh, 'textContent'))!;
+    expect(published_second_word).toBe(second_word);
+
+    page.off('request', failure_handler);
+
+    await page.setRequestInterception(false);
 }, timeout);
